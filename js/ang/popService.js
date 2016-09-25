56484081ab2424300c73ea2bea964aea5715abbf
mide <midesowunmi@yahoo.com>
2016-09-25 16:35:09 -0400

    Update popService.js

--- js/ang/popService.js
+++ js/ang/popService.js
@@ -1,1 +1,21 @@
-// popService allows calendar to communicate with addEvent pop up
+//broadcast service linking offlineController with addEventPop directive
+offlineController.factory('PopService', function($rootScope){
+    return{
+        popData: "",
+        eventService:function(mdata){
+             this.popData = mdata;
+            $rootScope.$broadcast("eventAdded");
+            console.log("broadcast..." + mdata);
+        },
+        eventStatus:function(status){
+            if(status == 'success'){
+                $rootScope.$broadcast("eventSaved");
+                console.log("broadcast... event saved " + status);
+            }
+            if(status == 'fail'){
+                $rootScope.$broadcast("eventFail");
+                console.log("broadcast... event saved " + status);
+            }
+
+        }
+    }
