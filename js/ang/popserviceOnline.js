d5bbf831d6ed9028030e591ed6b942cb9353f9c3
mide <midesowunmi@yahoo.com>
2016-09-25 17:04:55 -0400

    Create popserviceOnline.js

---
+++ js/ang/popserviceOnline.js
@@ -0,0 +1,23 @@
+//broadcast service linking offlineController with addEventPop directive
+onlineController.factory('PopServiceOnline', function ($rootScope) {
+    return{
+        popData: "",
+        eventService: function (mdata) {
+            this.popData = mdata;
+            $rootScope.$broadcast("eventAdded");
+            console.log("broadcast..." + mdata);
+        },
+        eventStatus:function(status){
+            if(status == 'success'){
+                $rootScope.$broadcast("eventSavedOnline");
+                console.log("broadcast... event saved " + status);
+            }
+            if(status == 'fail'){
+                $rootScope.$broadcast("eventFailOnline");
+                console.log("broadcast... event saved " + status);
+            }
+
+        }
+    }
+
+})
