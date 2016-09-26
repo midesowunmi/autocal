3573107c46ca25766375b9f30c3de5a18fc978b4
mide <midesowunmi@yahoo.com>
2016-09-26 18:56:08 -0400

    Update index.js

--- js/ang/online/index.js
+++ js/ang/online/index.js
@@ -2,4 +2,5 @@
 module.exports = angular.module('onlineModule',['util'])
 .config(require('./googleLoginProvider.js'))
 .controller('onlineCtrl', require('./autocalOnlineCtrl.js'))
-.directive('addEventPopOnlineDirective', require('./addEventPopOnlineDirecive.js'));
+.directive('addEventPopOnlineDirective', require('./addEventPopOnlineDirecive.js'))
+.factory('popServiceOnline', require('./popServiceOnline.js'));
