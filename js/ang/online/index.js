b2214d750b2bbb40998fe88797f3608fc4348a69
mide <midesowunmi@yahoo.com>
2016-09-26 18:53:21 -0400

    Update index.js

--- js/ang/online/index.js
+++ js/ang/online/index.js
@@ -1,4 +1,5 @@
 'use strict';
 module.exports = angular.module('onlineModule',['util'])
 .config(require('./googleLoginProvider.js'))
-.controller('onlineCtrl', require('./autocalOnlineCtrl.js');
+.controller('onlineCtrl', require('./autocalOnlineCtrl.js'))
+.directive('addEventPopOnlineDirective', require('./addEventPopOnlineDirecive.js'));
