49d614cca7b775a91c2d43cee734ab8575798b04
mide <midesowunmi@yahoo.com>
2016-09-26 18:48:54 -0400

    Update index.js

--- js/ang/online/index.js
+++ js/ang/online/index.js
@@ -1,4 +1,4 @@
 'use strict';
-module.exports = angular.module('onlineModule',['Util'])
+module.exports = angular.module('onlineModule',['util'])
 .config(require('./googleLoginProvider.js'))
 .controller('onlineCtrl', require('./autocalOnlineCtrl.js');
