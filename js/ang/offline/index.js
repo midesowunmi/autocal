149f409227b463acabff07b2bfbfa8c60d633dd6
mide <midesowunmi@yahoo.com>
2016-09-27 17:11:56 -0400

    Update index.js

--- js/ang/offline/index.js
+++ js/ang/offline/index.js
@@ -2,4 +2,5 @@
 module.exports = angular.module('offlineModule',['util'])
 .controller('offlineCtrl', require('./autocalOfflineCtrl.js'))
 .directive('addEventPopOfflineDirective', require('./addEventPopOfflineDirecive.js'))
-.factory('popServiceOffline', require('./services/popServiceOffline.js'));
+.factory('popServiceOffline', require('./services/popServiceOffline.js'))
+.factory('utilS', require('./services/utilService.js));
