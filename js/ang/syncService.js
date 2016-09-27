ac91633a56369272711d88029ccbbfabc8c48c4d
mide <midesowunmi@yahoo.com>
2016-09-27 17:07:32 -0400

    Delete syncService.js

--- js/ang/syncService.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var syncService = angular.module('syncService',[]);
-
-syncService.factory('Sync',['$scope',
-    function($scope){
-        console.log("init syncService");
-
-    }]);
