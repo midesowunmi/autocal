418d2eec86e0cc736723937dbf29d8040d85580d
mide <midesowunmi@yahoo.com>
2016-11-05 07:47:50 -0400

    Update addEventPopOnlineDirective.js

--- js/ang/online/addEventPopOnlineDirective.js
+++ js/ang/online/addEventPopOnlineDirective.js
@@ -3,14 +3,7 @@
 module.exports = function ($compile, PopServiceOnline, $rootScope) {
     return{
         restrict: 'AE',
-        template: '<div data-toggle="popover" class="event-popover col-sm-12">' +
-            '<div class="col-sm-12"><span class="col-sm-3">Date: </span><span class="col-sm-9">{{mydate}}</span></div>' +
-            '<div class="col-sm-12"><span class="col-sm-3">Time: </span><span class="col-sm-9"><input type="text" ng-model="eventstarttime"  class="texteventtime" id="eventstarttime"> &mdash; ' +
-            '<input type="text" id="eventendtime2" data-ng-model="eventendtime" class="texteventtime"></span></div>' +
-            '<div class="col-sm-12"><span class="col-sm-3">What:</span><span class="col-sm-9"> <input type="text" id="eventtitle" value="" data-ng-model="eventtitle" class="texteventtitle"> </span></div>' +
-            '<div class="col-sm-12"><span class="col-sm-3">Where:</span> <span class="col-sm-9"><input type="text" data-ng-model="eventlocation" value="" id="eventlocation" class="texteventtitle"> </span></div>' +
-            '<div class="col-sm-12"><span class="col-sm-offset-3 text-info" ng-bind="eventstatus"></span></div>'+
-            '<div class="col-sm-12"><span class="col-sm-offset-6"><span class="btn btn-danger" ng-click="closePop()">cancel</span><span class="btn btn-default" ng-click="sendEvent()" id="btnSavePop">save</span></span></div></div>',
+        template: '../templates/popver.html',
 
 
         scope:{
